9e456baec3b7b043e8160fa995483e0f4eb6fb9e
Owen Hills-Klaus <owhk2008@gmail.com>
2025-03-10 10:53:49 -0700

    Creates the connection pool

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import fastapi
 from fastapi import Request
+from contextlib import asynccontextmanager
+import asyncpg
 
-app = fastapi.FastAPI()
+# Create a FastAPI object with a init
 
+@asynccontextmanager
+async def lifespan(app: fastapi.FastAPI):
+    # Code to run on startup, initialize the postgres connection pool
+    print("Starting up...")
+    app.state.con = await asyncpg.create_pool("", max_size=20)
+    yield
+    # Code to run on shutdown
+    print("Shutting down...")
+
+app = fastapi.FastAPI(lifesan=lifespan)
 
 @app.post("/cheat_engine/fetch_answers")
 async def get_vhl_answers(request: Request):
