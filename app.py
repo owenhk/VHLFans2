fb42ebaaf185edcbf23dad061198429e79ac25af
Owen Hills-Klaus <owhk2008@gmail.com>
2025-03-10 13:08:55 -0700

    Just wasting time

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ app = fastapi.FastAPI(lifesan=lifespan)
 async def get_vhl_answers(request: Request):
     # Use WebEngine to decode the HTML of the page
     # Check CacheKit to see if the problem has already been solved
-    # Use AIEngine to determine how to solve each set of questions
-    # Pass AIEngine queries to QuizletEngine to get proper answers
+    # Pass each question to AIKit to solve, which will use QuizletKit to get the answers
     # Add a background worker to save results in CacheEngine
     # Pass the results back to the user, they cheated :)
     pass
